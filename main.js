0af3b5585367b35d340192e5ca69a2b360b4bf84
Yvette Skylar Amoth <yvetteamoth06@gmail.com>
2026-07-11 16:09:35 +0300

    Modify age and birthYear, add boolean type log
    Updated age and birthYear variables. Added a console log for a boolean type. Demonstrated let and const usage.

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 // Variable declarations
-let name = "Yvette Amoth";
-let age = 25;
-const birthYear = 1999;
+let name = "Yvette Amoth"; 
+let age = 22;
+const birthYear = 2005;
 
 // typeof operator
-console.log(typeof name);  // string
-console.log(typeof age);   // number
-console.log(typeof birthYear); // number
+console.log(typeof name);    // string
+console.log(typeof age);     // number
+console.log(typeof true);    // boolean
+
+// let vs const
+let score = 100;
+score = 150;  // Works!
+
+const PI = 3.14159;
+// PI = 3;  // Error! const cannot be reassigned
